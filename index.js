ea06e2f9d30f1cea380483a045bbfef80a2a1b08
Ryan Ramage <ryan.ramage@gmail.com>
2014-07-02 22:35:52 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,67 @@
+
+var url = require('url'),
+		_ = require('lodash'),
+		oboe = require('oboe'),
+    sjcl  = require('sjcl'),
+    xxtea = require('xxtea-xmlhttprequest');
+
+module.exports = function(base_url) {
+  
+  var api = {};
+
+  api.resolve = function(route) {
+  	return  url.resolve(base_url + '/', route);
+  }
+
+  api.package_details = function(cb){
+		oboe(this.resolve('package.json'))
+			.done(function(pkg){  cb(null, pkg) })
+			.fail(function(err){ cb(err); })
+  }
+
+  api.first_chapter = function(cb) {
+  	var self = this;
+
+  	this.package_details(function(err, pkg){
+  		if (err) return cb(err);
+      oboe(self.resolve('node/' + pkg.start_id))
+      	.fail(function(err){ return cb(err) })
+      	.done(function(start_chapter){
+      		cb(null, start_chapter);
+	      })
+  	})
+  }
+
+  api.crack_chapter = function(key_id, pass, cb) {
+  	var self = this;
+
+    oboe(self.resolve('key/' + key_id))
+      .fail(function(err){ return cb(err) })
+    	.done(function(key_ct){
+	      try {
+	        var c2 = JSON.parse( sjcl.decrypt(pass, JSON.stringify(key_ct)));
+	        oboe(self.resolve('node/' + c2.to))
+		        .done(function(chapter_ct){
+		          var chapter = JSON.parse( sjcl.decrypt(c2.key, JSON.stringify(chapter_ct)));
+		          chapter.id = c2.to;
+		          return cb(null, {key: c2.key, chapter: chapter});
+		        })
+	      } catch(e) {
+	        return cb(e);
+	      }
+	    })
+  }
+
+  api.blob = function(chapter, filename, key, cb) {
+  	var self = this,
+				file = _.find(chapter.files, function(file){ return file.name === filename })
+
+    xxtea(self.resolve('file/' + file.id), key, false, function(err, bytes){
+      var blob = new Blob([bytes], {type: file.content_type});
+      cb(null, blob);
+    })
+  }
+
+  return api;
+
+}
---
+++ tests/index.js
@@ -0,0 +1,47 @@
+var read = require('..')("http://t4.io/mrp9xw"),
+		test = require('tape');
+
+test('read package details', function(t){
+	t.plan(2);
+	read.package_details(function(err, pkg){
+		t.error(err);
+		t.equals(pkg.name, 'online');
+	})	
+})
+
+test('read first chapter', function(t){
+	t.plan(2);
+	read.first_chapter(function(err, chapter){
+		t.error(err);
+		t.equals(chapter.type, 'markdown');		
+	})
+})
+
+test('crack chapter', function(t){
+	t.plan(4);
+	read.crack_chapter('65c283d9-e6ac-4f27-9ef3-755bf8cf9d6e', '894', function(err, resp){
+		t.error(err);
+		t.equals(resp.key, 'iSSkNbKvWUm6pEDjUIEQwg==')
+		t.equals(resp.chapter.name, 'chapter2');
+		t.equals(resp.chapter.proof, '56e25f46-fae9-472d-98b0-68abeb753a49')
+	})
+})
+
+test('crack chapter, bad password', function(t){
+	t.plan(2);
+	read.crack_chapter('65c283d9-e6ac-4f27-9ef3-755bf8cf9d6e', '895', function(err, resp){
+		t.ok(err);
+		t.error(resp);
+	})
+})
+
+test('invalid chapter', function(t){
+	t.plan(2);
+	read.crack_chapter('fff283d9-e6ac-4f27-9ef3-755bf8cf9d6e', '895', function(err, resp){
+		t.ok(err);
+		t.error(resp);
+	})	
+})
+
+
+
